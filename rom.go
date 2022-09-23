f020d7ec4db531499ebec6455a42f65c20d89a24
David McCaffrey <redx@pop-os.localdomain>
2022-09-22 22:18:21 -0500

    Reorg ROM

--- rom.go
+++ rom.go
@@ -45,23 +45,27 @@ var progsMap = map[string]string {}
 
 
 func loadRom() {
-	err := filepath.Walk("rom/", func(path string, info os.FileInfo, err error) error {
-		if info.IsDir() {
-			return nil
-		}
-		fmt.Printf("\nLoading: path=%s", path)
-		data, err := os.ReadFile(path)
-		if err != nil {
-			return err
-		}
-		name := strings.Replace(path, "rom/", "", -1)
-		name = strings.Replace(name, ".28", "", -1)
-		prog := strings.Replace(string(data), "\n", "|", -1)
-		fmt.Printf("Loaded: name=%s, prog=%s", name, prog)
-		progsMap[name] = prog
-		return nil
-	})
+	err := filepath.Walk("rom/", loadFile)
+	fmt.Printf("\nprogsMap=%s", progsMap)
 	if err != nil {
 		fmt.Printf("\nFailed to read program: err=%s", err.Error())
 	}
+}
+
+func loadFile(path string, info os.FileInfo, err error) error {
+	if info.IsDir() {
+		return nil
+	}
+	fmt.Printf("\nLoading: path=%s", path)
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+	name := strings.Replace(path, "rom/", "", -1)
+	name = strings.Replace(name, ".28", "", -1)
+	name = strings.ToUpper(name)
+	prog := strings.Replace(string(data), "\n", "|", -1)
+	fmt.Printf("Loaded: name=%s, prog=%s", name, prog)
+	progsMap[name] = prog
+	return nil
 }
